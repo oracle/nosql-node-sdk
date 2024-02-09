491642662f95f03c6bdfedfa998005f7b5e8e910
Yevgeniy Polonskiy <50853059+y-polonsk@users.noreply.github.com>
2024-02-09 06:05:27 -0800

    Updated copyrights and changelog

--- test/unit/null_rate_limiter.js
+++ test/unit/null_rate_limiter.js
@@ -1,3 +1,10 @@
+/*-
+ * Copyright (c) 2018, 2024 Oracle and/or its affiliates. All rights reserved.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ *  https://oss.oracle.com/licenses/upl/
+ */
+
 'use strict';
 
 //Fake rate limiter class for testing
